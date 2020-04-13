6dd85f6babb2b4e4ebbc8707f595055de04b1aa5
nextzuckerberg <ac1895@georgetown.edu>
2020-04-13 18:36:52 -0400

    Basic functions with tests

---
+++ my_script.py
@@ -0,0 +1,12 @@
+# testing-123/my_script.py
+
+def enlarge(i):
+    return i * 100
+
+
+
+if __name__ == "__main__":
+    my_number = float(input("Input a number"))
+    n = enlarge(my_number)
+
+    print(n)
---
+++ my_test.py
@@ -0,0 +1,7 @@
+# testing-123/my_test.py
+
+from my_script import enlarge
+
+def test_enlarge():
+    result = enlarge(3)
+    assert result == 300
